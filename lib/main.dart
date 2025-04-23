1eaffff5412ea3065f06cfa17bf3575e12ed15bd
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-22 22:21:13 -0300

    votacaoPets

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,101 @@
 import 'package:flutter/material.dart';
- 
-void main() => runApp(MyApp());
- 
-class MyApp extends StatelessWidget {
+
+void main() => runApp(VotacaoApp());
+
+class VotacaoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Contador());
+    return MaterialApp(home: VotacaoPage());
   }
 }
- 
-class Contador extends StatefulWidget {
+
+class VotacaoPage extends StatefulWidget {
   @override
-  State<Contador> createState() => _ContadorState();
+  State<VotacaoPage> createState() => _VotacaoPageState();
 }
 
-class _ContadorState extends State<Contador> {
-  int contador = 0;
+class _VotacaoPageState extends State<VotacaoPage> {
+  int contadorGato = 0;
+  int contadorCao = 0;
+
+  // Função para determinar o vencedor
+  String getWinnerMessage() {
+    if (contadorCao > contadorGato) {
+      return 'Cachorros estão vencendo!';
+    } else if (contadorGato > contadorCao) {
+      return 'Gatos estão vencendo!';
+    } else {
+      return 'A votação está empatada!';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Contador com Estado')),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('Valor: $contador', style: TextStyle(fontSize: 32, color: Colors.black)),
-          SizedBox(height: 30),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(onPressed: increment, child: Text('+')),
-              SizedBox(width: 10),
-              ElevatedButton(onPressed: decrement, child: Text('-')),
-              SizedBox(width: 10),
-              ElevatedButton(onPressed: reset, child: Text('Resetar')),
-            ],
-          ),
-        ],
+      appBar: AppBar(title: Text("Simulador de Votação")),
+      body: Padding(
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '🐱 Gatos: $contadorGato   🐶 Cachorros: $contadorCao',
+              style: TextStyle(fontSize: 22),
+            ),
+            SizedBox(height: 16),
+            Text(
+              "Resultado",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 32),
+            Text(
+              getWinnerMessage(),
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+            ),
+            SizedBox(height: 32),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                    onPressed: incrementCat, child: Text("Votar em Gatos")),
+                SizedBox(width: 16),
+                ElevatedButton(
+                  onPressed: incrementDog,
+                  child: Text("Votar em Cachorros"),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: reset,
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.grey),
+              child: Text("Resetar Votação"),
+            ),
+          ],
+        ),
       ),
     );
   }
 
-  void increment () {
+  // Função para incrementar o contador de votos de gatos
+  void incrementCat() {
     setState(() {
-      contador ++;
+      contadorGato++;
     });
   }
-  void decrement () {
+
+  // Função para incrementar o contador de votos de cachorros
+  void incrementDog() {
     setState(() {
-      contador --;
+      contadorCao++;
     });
   }
-  void reset () {
+
+  // Função para resetar os contadores
+  void reset() {
     setState(() {
-      contador = 0;
+      contadorCao = 0;
+      contadorGato = 0;
     });
   }
 }
- 
- 
